3544892b6c992f7b281e34af31f668c39421aecf
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2022-10-17 19:35:41 +0300

    bagfix_2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,7 +27,7 @@ export const App = () => {
             <Route path="cast" element={<Cast/>} />
             <Route path="reviews" element={<Reviews/>} />
           </Route>
-          <Route path="*" element={<Home/>} />
+          <Route path="*" element={<Navigate to="/home"/>} />
         </Route>
       </Routes>
     </div>
